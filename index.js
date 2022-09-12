1ecc79c87f70fc7b75230394a1423960e6a26741
yonasketema <yonask.kitaw@gmail.com>
2022-09-12 19:03:16 +0300

    init mongoose

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+require("dotenv").config({ path: ".env" });
 const express = require("express");
+const mongoose = require("mongoose");
+
+const userRouter = require("./routes/userRouter");
 
 app = express();
 
-const PORT = process.env.PORT || 8000;
+app.use(express.json());
 
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("> DB connection successful ! ");
+  });
+
+app.use("/api/v1/users", userRouter);
+
+const PORT = process.env.PORT || 8000;
 app.listen(8000, () => {
   console.log(`> Server running on ${PORT}`);
 });
