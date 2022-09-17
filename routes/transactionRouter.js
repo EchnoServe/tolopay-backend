688a9253057eaa7a70fddd2a14f7ef55848c5d20
yonasketema <yonask.kitaw@gmail.com>
2022-09-17 20:55:50 +0300

    feat:user transaction

--- routes/transactionRouter.js
+++ routes/transactionRouter.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const transactionController = require("./../controllers/transactionController");
+const { protect } = require("./../middleware/protect");
 
 const router = express.Router();
 
-router.post("/transfer", transactionController.transfer);
-// router.get("/usertransactions", transactionController.usertransactions);
+router.post("/transfer", protect, transactionController.transfer);
+router.get(
+  "/usertransactions",
+  protect,
+  transactionController.usertransactions
+);
 
 module.exports = router;
