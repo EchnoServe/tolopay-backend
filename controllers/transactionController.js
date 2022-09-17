954a64868e8d6670b912a1d48abb21dda76c0bab
yonasketema <yonask.kitaw@gmail.com>
2022-09-17 20:36:04 +0300

    fix:error handling and add transactions

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,68 +1,58 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("./../models/user");
 const CreditTransaction = require("./../models/creditTransaction");
 const DebitTransaction = require("./../models/debitTransaction");
-const bcrypt = require("bcryptjs");
+const user = require("./../models/user");
 
 //when two companies transact with one another say Company A buys something from Company B then Company A will record a decrease in cash (a Credit), and Company B will record an increase in cash (a Debit).
-// ! status code
+// TODO : status code  ! status
 
-const user_id = "6323d5a842a651236f124d46"; //sender
+/**
+ *  @desc transfer money
+ *  @route POST /api/v1/transaction/transfer
+ *  @access Private
+ */
 
 exports.transfer = async (req, res, next) => {
-  //? check the user id object.type.id
-  //check if their is a given  username / user_id
+  //TODO: transfer by account number or phone number
 
+  //check if the user input all fields
+  const user = req.user;
   const { receiver_user_email, amount, password, remark } = req.body;
   if (!receiver_user_email || !amount || !password) {
-    return next(new Error("enter the requirement"));
+    return next(new Error("Please enter the fields in your form correctly"));
   }
 
+  //check if  a given  receiver exists
   const receiver_user = await User.findOne({ email: receiver_user_email });
   if (!receiver_user) {
-    return next(new Error("their is no  a user with is email"));
+    return next(new Error("Please enter correct email "));
   }
-  //?// check the user  balance is gook
-  const send_user = await User.findById(user_id).select("+password");
-
-  if (send_user.balance < amount) {
-    return next(new Error("In balance ", 404)); //bad req
+  //?// check the user  balance is good
+  //if a user has not enough balance to send
+  if (user.balance < amount) {
+    return next(new Error("your balance is not enough"));
   }
 
-  //!check if the password is correct
-  //attempt ??????
-  //attempt = {
-  // date:Date.now(),
-  //attempt_number:1++,
-  //}
+  //TODO: wrong password  attempts
 
-  /*
-    if(attempt.date === Date && attempt_number > 7){
-      user.active = false,
-      user.save()
-    }
-    */
-
-  if (!send_user || !(await bcrypt.compare(password, send_user.password))) {
-    return next(new Error("Incorrect password", 401));
+  //validate the password is correct
+  if (!(await bcrypt.compare(password, user.password))) {
+    return next(new Error("Incorrect password"));
   }
 
   //  the balance - and +
-  const previousAmount = send_user.balance;
-  const remainingAmount = send_user.balance - amount;
-  const update_user = await User.findByIdAndUpdate(send_user.id, {
+  const previousAmount = user.balance;
+  const remainingAmount = user.balance - amount;
+  await User.findByIdAndUpdate(user.id, {
     balance: remainingAmount,
   }); //
-  ///
-  const receiver_user_previousAmount = parseFloat(receiver_user.balance);
-  const currentAmount = parseFloat(receiver_user.balance) + parseFloat(amount);
 
-  const update_reciever_user = await User.findByIdAndUpdate(receiver_user._id, {
-    balance: currentAmount,
-  }); //+balance
-
-  // create credit and debit transaction//
+  //
+  // create   transaction//
   const creditTransaction = await CreditTransaction.create({
-    user_id: user_id,
+    user_id: user.id,
     remark: remark,
     receiver_user: receiver_user._id,
     transferAmount: amount,
@@ -71,17 +61,21 @@ exports.transfer = async (req, res, next) => {
     type: "credit",
   });
 
-  const debitTransaction = await DebitTransaction.create({
-    user_id: receiver_user._id,
-    send_user: user_id,
+  //
+  const receiver_user_previousAmount = parseFloat(receiver_user.balance);
+  const currentAmount = parseFloat(receiver_user.balance) + parseFloat(amount);
+  await User.findByIdAndUpdate(receiver_user._id, {
+    balance: currentAmount,
+  }); //+balance TODO:.save()
+
+  await DebitTransaction.create({
+    user_id: receiver_user.id,
+    send_user: user.id,
     transferAmount: amount,
     previousAmount: receiver_user_previousAmount,
     currentAmount: currentAmount,
     type: "debit",
   });
-
-  // balance
-  //res ->credit
 
   res.status(201).json({
     status: "OK",
@@ -91,13 +85,28 @@ exports.transfer = async (req, res, next) => {
   next();
 };
 
+/**
+ *  @desc all user debit and credit transactions
+ *  @route POST /api/v1/transaction/usertransactions
+ *  @access Private
+ */
+
 exports.usertransactions = async (req, res, next) => {
-  const user = await User.findById(user_id)
+  //TODO:multiple populate may decrease performance ?aggregate
+
+  const user = await User.findById(req.user.id)
     .populate("creditTransactions")
     .populate("debitTransactions");
 
+  const transaction = [
+    ...user.creditTransactions,
+    ...user.debitTransactions,
+  ].sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  });
+
   res.status(200).json({
-    user,
+    transaction,
   });
 
   next();
