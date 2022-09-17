a89c2755c8a59f8a73eb5da70ad9ea7535ce16f3
yonasketema <yonask.kitaw@gmail.com>
2022-09-17 10:38:59 +0300

    fix:error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,65 +8,47 @@ const signToken = (id) => {
   });
 };
 
-exports.signup = async (req, res) => {
-  try {
-    const { name, email, password, passwordConfirm, phoneNumber } = req.body;
+exports.signup = async (req, res, next) => {
+  const { name, email, password, passwordConfirm, phoneNumber } = req.body;
 
-    const user = await User.create({
-      name,
-      email,
-      password,
-      passwordConfirm,
-      phoneNumber,
-    });
+  const user = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+    phoneNumber,
+  });
 
-    const token = signToken(user._id);
+  const token = signToken(user._id);
 
-    res.status(201).json({
-      status: "OK",
-      data: {
-        token,
-        user: user._id,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      data: {
-        errorMessage: error.message,
-      },
-    });
-  }
+  res.status(201).json({
+    status: "OK",
+    data: {
+      token,
+      user: user._id,
+    },
+  });
 };
 
 exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-      return next(new Error("provide email and password"));
-    }
-
-    const user = await User.findOne({ email }).select("+password");
-
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return next(new Error("incorrect email or password"));
-    }
-
-    const token = signToken(user._id);
-    res.status(200).json({
-      status: "OK",
-      data: {
-        token,
-        user: user._id,
-      },
-    });
-  } catch (e) {
-    res.status(404).json({
-      status: "fail",
-      data: {
-        errorMessage: e.message,
-      },
-    });
+  if (!email || !password) {
+    return next(new Error("provide email and password"));
   }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return next(new Error("incorrect email or password"));
+  }
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "OK",
+    data: {
+      token,
+      user: user._id,
+    },
+  });
 };
