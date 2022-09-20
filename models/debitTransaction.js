1987c47218790c610c6a35d75b463c262ad9db4b
yonasketema <yonask.kitaw@gmail.com>
2022-09-20 11:27:21 +0300

    feat: populate sender user name

--- models/debitTransaction.js
+++ models/debitTransaction.js
@@ -9,11 +9,23 @@ const transactionSchema = new mongoose.Schema({
     type: Date,
     default: new Date(),
   },
-  send_user: String,
+  send_user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    require: true,
+  },
   transferAmount: Number,
   previousAmount: Number,
   currentAmount: Number,
   type: String,
+});
+
+transactionSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "send_user",
+    select: "name",
+  });
+  next();
 });
 
 const debitTransactionModel = mongoose.model(
