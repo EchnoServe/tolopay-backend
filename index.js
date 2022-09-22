be8ef58fdf0a87f31c468f9c7b59443f4b2b53e6
Melat <melatkassaye13@gmail.com>
2022-09-22 15:36:52 +0300

    added cors

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-require("dotenv").config({ path: ".env" });
+
 require("express-async-errors");
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors=require('cors');
+const dotenv = require("dotenv");
 const { error } = require("./middleware/error");
 const userRouter = require("./routes/userRouter");
 const transactionRouter = require("./routes/transactionRouter");
 
 const app = express();
 
-app.use(express.json());
 
+
+dotenv.config();
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
@@ -19,13 +21,14 @@ mongoose
   .then(() => {
     console.log("> DB connection successful ! ");
   });
-
+app.use(express.json());
+app.use(cors());
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/transaction", transactionRouter);
 
 app.use(error);
 
-const PORT = process.env.PORT || 8000;
-app.listen(8000, () => {
-  console.log(`> Server running on ${PORT}`);
+
+app.listen(process.env.PORT||  8000,() => {
+  console.log(`> Server running on port 8000`);
 });
