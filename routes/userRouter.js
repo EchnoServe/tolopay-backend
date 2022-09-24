81a1199c1db902b539249345f7d2ef9d0f6e5568
yonasketema <yonask.kitaw@gmail.com>
2022-09-24 17:40:37 +0300

    feat: add GET /me profile route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
+router.get("/me", protect, userController.me);
+
 router.put("/addbudget", protect, userController.addBudget);
 
 module.exports = router;
