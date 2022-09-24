145bf0102ff1fa541c2d6aec76af05775133abd3
yonasketema <yonask.kitaw@gmail.com>
2022-09-24 19:41:22 +0300

    add passport

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("express-async-errors");
 const express = require("express");
 const mongoose = require("mongoose");
 
+const cors = require("cors");
 const bodyParser = require("body-parser");
- 
+
 const dotenv = require("dotenv");
 const { error } = require("./middleware/error");
 const userRouter = require("./routes/userRouter");
@@ -26,7 +27,7 @@ app.use("/tolopayprofiles", express.static("tolopayprofiles"));
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api/v1/users", userRouter);
