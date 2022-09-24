af668cb63958f238d26c204a18a1c35bd1dbda66
yonasketema <yonask.kitaw@gmail.com>
2022-09-24 09:31:23 +0300

    feat:add addbudget route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { protect } = require("../middleware/protect");
 const authController = require("./../controllers/authController");
+const userController = require("./../controllers/userController");
 
 const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+
+router.put("/addbudget", protect, userController.addBudget);
 
 module.exports = router;
