c54b850340d4d6d06aef66203ff993402a2e4351
yonasketema <yonask.kitaw@gmail.com>
2022-10-02 22:31:25 +0300

    feat:recurrenttransaction

---
+++ controllers/recurrentTransactionController.js
@@ -0,0 +1,21 @@
+const RecurrentTransaction = require("./../models/recurrenttransaction");
+
+exports.addRecurrentTransaction = async (req, res, next) => {
+  const { date, amount, account_number_to, remark } = req.body;
+
+  const recurrent = await RecurrentTransaction.create({
+    date: Date.now() + date * 60 * 1000,
+    recurrent: date,
+    amount,
+    account_number_from: req.user.id,
+    account_number_to,
+    remark,
+  });
+
+  res.status(201).json({
+    status: "OK",
+    data: {
+      recurrent,
+    },
+  });
+};
